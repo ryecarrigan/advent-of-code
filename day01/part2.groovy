6ba081ff45506a04210469d6ba0e5aae7403751f
Ryan Carrigan <r.carrigan@arca.com>
2018-12-02 07:33:02 -0500

    Adding .gitignore and a small style fix

--- day01/part2.groovy
+++ day01/part2.groovy
@@ -1,6 +1,6 @@
 def freq   = 0
 def freqs  = new LinkedHashSet()
-def values = new File('input.txt').readLines().collect { line -> line.toInteger()}
+def values = new File('input.txt').readLines().collect { line -> line.toInteger() }
 
 while (true) {
     for (value in values) {
